d4f85a47982bcf19f7b72ef3b1ce638380913b68
Manuel Dominguez <manuel.dogo@gmail.com>
2024-12-04 22:51:04 +0100

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule AdventOfCode.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :advent_of_code,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :inets, :public_key, :ssl]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:benchee, "~> 1.0"},
+      {:credo, "~> 1.7"},
+      {:dialyxir, "~> 1.4"},
+      {:ssl_verify_fun, "~> 1.1.7", manager: :rebar3, override: true}
+    ]
+  end
+end
